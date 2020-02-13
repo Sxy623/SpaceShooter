8c793e6bd0d8c99b98f0ab2a367d82aa668560a8
Sxy623 <191436989@qq.com>
2020-02-14 01:40:34 +0800

    Asteroid

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Boundary")) return;
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
