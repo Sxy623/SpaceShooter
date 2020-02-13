116f9afd3ff64ce2a471aed55340f96e47dd85a9
Sxy623 <191436989@qq.com>
2020-02-14 01:56:52 +0800

    Explosion

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -2,8 +2,17 @@
 
 public class DestroyByContact : MonoBehaviour
 {
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Boundary")) return;
+        var selfTransform = transform;
+        Instantiate(explosion, selfTransform.position, selfTransform.rotation);
+        if (other.CompareTag("Player")) {
+            var otherTransform = other.transform;
+            Instantiate(playerExplosion, otherTransform.position, otherTransform.rotation);
+        }
         Destroy(other.gameObject);
         Destroy(gameObject);
     }
